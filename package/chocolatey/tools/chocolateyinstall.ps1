23d291c8dee9504c868a7d17d9c268f41c8d468c
Michael Lohr <michael@lohr.dev>
2021-08-09 18:37:13 +0200

    Finish choco package

--- package/chocolatey/tools/chocolateyinstall.ps1
+++ package/chocolatey/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 $ErrorActionPreference = 'Stop';
 
 $packageName = 'punktf'
-$destDir     = Join-Path $(Get-ToolsLocation) $packageName
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64       = 'https://github.com/Shemnei/punktf/releases/download/v1.0.0-alpha/punktf-x86_64-pc-windows-msvc.zip'
-
+$bin         = $toolsDir + '\punktf-x86_64-pc-windows-msvc\punktf.exe'
 
 $packageArgs = @{
     packageName   = $packageName
@@ -12,12 +12,13 @@ $packageArgs = @{
 
     softwareName  = 'punktf*'
 
-    unzipLocation = $destDir
+    unzipLocation = $toolsDir
 
     checksum64    = 'e4cf1d9ed43217df69b7e13659435b86614ac62ea6049fc079785e993b8c5b0f'
     checksumType64= 'sha256'
 
     validExitCodes= @(0)
 }
-
 Install-ChocolateyZipPackage @packageArgs
+
+Install-BinFile -Name "punktf" -Path "$bin"
