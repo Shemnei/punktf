89408f16c4f0c4403647ad2bfdaa901a264630b3
Michael Lohr <michael@lohr-ffb.de>
2021-08-10 14:58:23 +0200

    Update punktf-bin.rb

--- package/homebrew/punktf-bin.rb
+++ package/homebrew/punktf-bin.rb
@@ -1,11 +1,9 @@
-class PunktfBin < Formula
-  version 'v1.0.0-alpha'
+class Punktf < Formula
   desc "A cross-platform multi-target dotfiles manager"
   homepage "https://github.com/Shemnei/punktf"
-
-  if OS.linux?
-    url "https://github.com/Shemnei/punktf/releases/download/#{version}/punktf-x86_64-unknown-linux-gnu.tar.gz"
-  end
+  version "v1.0.0-alpha"
+  url "https://github.com/Shemnei/punktf/releases/download/#{version}/punktf-x86_64-unknown-linux-musl.tar.gz"
+  sha256 "88b9e22770c6dd1d44843e145b617f31347a17c47ff557385ac958df12a8d87d"
 
   conflicts_with "punktf"
 
