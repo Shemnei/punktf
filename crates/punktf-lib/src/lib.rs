16eddc994b46a10359e11008d3b3133f14835ed6
Shemnei <Shemnei@users.noreply.github.com>
2021-08-18 17:54:50 +0200

    Added attribute to deny broken doc links

--- crates/punktf-lib/src/lib.rs
+++ crates/punktf-lib/src/lib.rs
@@ -10,6 +10,7 @@
     future_incompatible,
     missing_copy_implementations,
     //rustdoc::missing_crate_level_docs,
+	rustdoc::broken_intra_doc_links,
     //missing_docs,
     //clippy::missing_docs_in_private_items,
     missing_debug_implementations,
