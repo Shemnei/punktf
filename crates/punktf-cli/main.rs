0a6b0e0fc24734b721718e752e9dbf51fb301d1f
Shemnei <Shemnei@users.noreply.github.com>
2021-08-20 11:33:47 +0200

    punktf-cli: Added attribute from punktf-lib to main.rs

--- crates/punktf-cli/main.rs
+++ crates/punktf-cli/main.rs
@@ -99,6 +99,36 @@
 //!
 //! - [michidk/dotfiles](https://gitlab.com/michidk/dotfiles)
 
+#![allow(rustdoc::private_intra_doc_links)]
+#![deny(
+	dead_code,
+	deprecated_in_future,
+	exported_private_dependencies,
+	future_incompatible,
+	missing_copy_implementations,
+	rustdoc::missing_crate_level_docs,
+	rustdoc::broken_intra_doc_links,
+	missing_docs,
+	clippy::missing_docs_in_private_items,
+	missing_debug_implementations,
+	private_in_public,
+	rust_2018_compatibility,
+	rust_2018_idioms,
+	trivial_casts,
+	trivial_numeric_casts,
+	unsafe_code,
+	// TODO(future): would be nice in the future but not possible for now
+	// unstable_features,
+	unused_import_braces,
+	unused_qualifications,
+
+	// clippy attributes
+	clippy::missing_const_for_fn,
+	clippy::redundant_pub_crate,
+	clippy::use_self
+)]
+#![cfg_attr(docsrs, feature(doc_cfg), feature(doc_alias))]
+
 mod opt;
 mod util;
 
