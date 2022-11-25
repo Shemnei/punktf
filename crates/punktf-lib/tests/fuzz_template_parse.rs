8811d672d00f4f63151fbafbec4ea38dccf0371a
Jonas Grawe <Shemnei@users.noreply.github.com>
2022-11-26 00:47:33 +0100

    Fixed fuzz artifact fuzz_template_parse/minimized-from-99658ac1fce12b1bd80cfc1d5219cf49284b473a

--- crates/punktf-lib/tests/fuzz_template_parse.rs
+++ crates/punktf-lib/tests/fuzz_template_parse.rs
@@ -1,7 +1,10 @@
 /// Tests for cases discovered by fuzzing this crate.
 /// The only checks done are, that no panic occurs.
 
-const TEMPLATES: &[&[u8]] = &[&[9, 123, 123, 10, 125, 125, 26]];
+const TEMPLATES: &[&[u8]] = &[
+	// fuzz/artifacts/fuzz_template_parse/minimized-from-99658ac1fce12b1bd80cfc1d5219cf49284b473a
+	&[9, 123, 123, 10, 125, 125, 26]
+];
 
 use punktf_lib::template::source::Source;
 use punktf_lib::template::Template;
