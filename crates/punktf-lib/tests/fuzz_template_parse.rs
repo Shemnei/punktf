b7302b056734f2b11f288c7053bc1f66864f8097
Jonas Grawe <Shemnei@users.noreply.github.com>
2022-11-26 01:42:52 +0100

    clippy

--- crates/punktf-lib/tests/fuzz_template_parse.rs
+++ crates/punktf-lib/tests/fuzz_template_parse.rs
@@ -3,7 +3,7 @@
 
 const TEMPLATES: &[&[u8]] = &[
 	// fuzz/artifacts/fuzz_template_parse/minimized-from-99658ac1fce12b1bd80cfc1d5219cf49284b473a
-	&[9, 123, 123, 10, 125, 125, 26]
+	&[9, 123, 123, 10, 125, 125, 26],
 ];
 
 use punktf_lib::template::source::Source;
