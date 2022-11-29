250522eb4e92586cdb6afc34b93a29231e2bef3e
Jonas Grawe <Shemnei@users.noreply.github.com>
2022-11-29 16:44:17 +0100

    Improved ci script

--- examples/ci.sh
+++ examples/ci.sh
@@ -7,6 +7,9 @@ for path in *; do
 	# if not a directory, skip
     [[ -d "${path}" ]] || continue
 
+	# if no `run.sh` in directory, skip
+	[[ -f "${path}/run.sh" ]] || continue
+
     dirname="$(basename "${path}")"
 
 	pushd "${dirname}"
